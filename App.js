64c87fa3413efbbf8520c7580c4e7f117b16ed58
Ricky Chan <ric0611@gmail.com>
2020-01-24 23:41:34 -0800

    bottom nav bar added icons and styles

--- App.js
+++ App.js
@@ -14,11 +14,14 @@ import AsyncStorage from '@react-native-community/async-storage';
 import { FavesContext } from './src/context/FavesContext'
 import FavesProvider from './src/context/FavesContext'
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCalendarAlt, faHeart, faMap, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+
  // TODO: Remove when fixed
  YellowBox.ignoreWarnings([
   'VirtualizedLists should never be nested',
   'Remote debugger is in a background tab which may cause apps to perform slowly. Fix this by foregrounding the tab (or opening it in a separate window).',
-  'Calling bridge.image.Loader '
+'Calling bridge.imageLoader is deprecated and will not work in newer versions of RN. Please update to themoduleForClass API or turboModuleLookupDelegate API'
 ])
 
 const ScheduleStack = createStackNavigator(
@@ -32,21 +35,71 @@ const ScheduleStack = createStackNavigator(
     }
 );
 
+const IconComponent = props => {
+  return(
+    <FontAwesomeIcon icon={faHeart} style={styles.Heart} size={20} />
+  )
+}
+
 const BottomNav = createBottomTabNavigator({
-  Map,
   Schedule: {
-    screen: ScheduleStack
+    screen: ScheduleStack,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesomeIcon icon={faCalendarAlt} color={tintColor} size={30} />
+        ),
+      },
+    },
+    Map:{
+      screen: Map,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesomeIcon icon={faMap} color={tintColor} size={30} />
+        ),
+      },
+    },
+    Faves:{
+      screen: Faves,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesomeIcon icon={faHeart} color={tintColor} size={30} />
+        ),
+      },
+    },
+    About: {
+      screen : About,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesomeIcon icon={faExclamationCircle} color={tintColor} size={30} />
+        ),
+      },
+    }
+},
+{
+  tabBarOptions: {
+    activeTintColor: 'white',
+    inactiveTintColor: 'gray',
+    labelStyle: {
+      fontSize: 16,
+      margin: 10
+    },
+    style: {
+      backgroundColor: 'black',
+      color: 'white',
+      height: 110,
+      paddingBottom: 30,
+      paddingTop: 15,
+    },
+    safeAreaInset: {
+      bottom: 'never'
+    },
   },
-  Faves,
-  About
 },
 StatusBar.setBarStyle('light-content', true)
 );
 
 const AppNavigation = createAppContainer(BottomNav);
 const App = () => {
-  // const [faveIds, setFaveIds] = useContext(FavesContext)
-
 return(
     <FavesProvider>
       <AppNavigation />
